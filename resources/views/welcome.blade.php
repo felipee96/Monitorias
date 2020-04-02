5e3e951a779a11c461484ed51e7e8cac8262e7f7
felipee96 <afgb1996@gmail.com>
2020-04-02 00:27:09 -0500

    ensayo de git visual

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <div class="setion">
       <img class="sala" src="imagenes/monitor.png">
       <h2>USUARIO</h2>
-      <p>Seccion de ingreso para monitores</p>
+      <p>Seccion de ingreso para monitores felipe</p>
       <a href="/login"><button class="buttonReservar">Ingresar </button></a>
     </div>
   </div>
