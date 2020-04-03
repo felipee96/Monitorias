23180d4667158ec01a7cb1ad71f59fbc77cb9bbc
PABLO YAIME <jpyaimen@corhuila.edu.co>
2020-04-02 19:51:01 -0500

    Se cambio el nombre del admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="setion">
       <img class="sala" src="imagenes/administrador.png">
       <h2>ADMINISTRADOR</h2>
-      <p>Seccion de ingreso para administrador Alberto Boalaños castro</p>
+      <p>Seccion de ingreso para administrador Juan Pablo Yaime</p>
       <a href="/login"><button class="buttonReservar">Ingresar </button></a>
     </div>
     <div class="setion">
