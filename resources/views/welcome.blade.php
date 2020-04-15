c712ab1f4df960c26edceaec1761c95fba0c96ff
PABLO YAIME <jpyaimen@corhuila.edu.co>
2020-04-15 14:50:05 -0500

    Se quito el nombre de admin y usuario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,13 @@
     <div class="setion">
       <img class="sala" src="imagenes/administrador.png">
       <h2>ADMINISTRADOR</h2>
-      <p>Seccion de ingreso para administrador Juan Pablo Yaime</p>
+      <p>Seccion de ingreso para administrador</p>
       <a href="/login"><button class="buttonReservar">Ingresar </button></a>
     </div>
     <div class="setion">
       <img class="sala" src="imagenes/monitor.png">
       <h2>USUARIO</h2>
-      <p>Seccion de ingreso para monitores felipee</p>
+      <p>Seccion de ingreso para monitores</p>
       <a href="/login"><button class="buttonReservar">Ingresar </button></a>
     </div>
   </div>
